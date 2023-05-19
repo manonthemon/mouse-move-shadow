d250ee3cea8e91ac1b94529b5b209041057fb4bf
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-19 10:55:17 +0100

    Stylized text shadow

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 const text = document.querySelector("h1");
-const walk = 100; //How far should the shadow go == 100px
+const walk = 20; //How far should the shadow go == 100px
 
 
 function shadow(e) {
@@ -16,7 +16,14 @@ function shadow(e) {
   const xWalk = Math.round(x / width * walk) - (walk / 2) //Math round round down the numbers
   const yWalk = Math.round(y / height * walk) - (walk / 2)
 
-text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`
+text.style.textShadow = `
+${xWalk}px ${yWalk}px rgba(255,0,255,0.7),
+${xWalk * -1}px ${yWalk}px rgba(0, 255, 255,0.7),
+${yWalk}px ${xWalk * -1}px rgba(0, 255, 0,0.7),
+${yWalk * -1}px ${xWalk}px rgba(0, 0, 255,0.7)
+
+
+`
 }
 
 hero.addEventListener("mousemove", shadow);
